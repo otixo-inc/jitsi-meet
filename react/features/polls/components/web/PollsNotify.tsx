9badf6afacdd12e317bd59f34bb062f6d4c2008b
Jamie <jamie@otixo.com>
2024-10-22 13:26:03 +0200

    wip

---
+++ react/features/polls/components/web/PollsNotify.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { IReduxState } from '../../../app/types';
+
+const PollsNotify = () => {
+    const polls = useSelector((state: IReduxState) => state['features/polls'].polls);
+
+    React.useEffect(()=>{
+      if (typeof APP !== 'undefined') {
+        APP.API.pollResultsChanged(polls)
+      }
+      console.debug(polls);
+    }, [polls])
+
+    return null;
+};
+
+export default PollsNotify
