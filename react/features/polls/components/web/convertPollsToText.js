aabbf0c1ab5128011b966087aad92887cb0e400a
Jamie <jamie@otixo.com>
2024-10-23 12:04:13 +0200

    wip

--- react/features/polls/components/web/convertPollsToText.js
+++ react/features/polls/components/web/convertPollsToText.js
@@ -1,34 +1,24 @@
 export const convertPollsToText = polls => {
   return Object.values(polls).reduce((previous, poll, index, arr) => {
-      const allVoters = new Set();
+      const voterSet = new Set();
 
       // Getting every voters ID that participates to the poll
       for (const answer of poll.answers) {
-          // checking if the voters is an array for supporting old structure model
-          const voters = answer.voters?.length
-              ? answer.voters
-              : Object.keys(answer.voters);
-
-          voters.forEach((voter) => allVoters.add(voter));
+          for (const [ voterId ] of answer.voters) {
+              voterSet.add(voterId);
+          }
       }
+
+      const totalVoters = voterSet.size;
 
       const { answers } = poll;
 
-      answers.sort((a, b) => b.voters.length - a.voters.length);
+      answers.sort((a, b) => b.voters.size - a.voters.size);
 
       const answersText = answers
           .map((answer) => {
-              const nrOfVotersPerAnswer = answer.voters
-                  ? Object.keys(answer.voters).length
-                  : 0;
-              const percentage =
-                  allVoters.size > 0
-                      ? Math.round(
-                            (nrOfVotersPerAnswer / allVoters.size) * 100
-                        )
-                      : 0;
-
-              return `${answer.name}: ${answer.voters.length} (${percentage}%)
+              const percentage = totalVoters === 0 ? 0 : Math.round(answer.voters.size / totalVoters * 100);              
+              return `${answer.name}: ${answer.voters.size} (${percentage}%)
 `;
           })
           .join("");
