e6c9ea7060bcc61e6e35ebf36d8a07a235b95582
Andrey Sotnikov <andrey.sotnikov@ventionteams.com>
2025-11-04 12:35:05 +0300

    Revert "feat(visitors): Adds support for visitors voting in polls."
    This reverts commit 5d1236534cb5fd18e35f289b2760792603c80b13.

--- react/features/conference/functions.any.ts
+++ react/features/conference/functions.any.ts
@@ -1,5 +1,7 @@
 import { IStateful } from '../base/app/types';
 import { toState } from '../base/redux/functions';
+import { iAmVisitor } from '../visitors/functions';
+
 
 /**
  * Tells whether or not the notifications should be displayed within
@@ -34,5 +36,5 @@ export function arePollsDisabled(stateful: IStateful) {
         return true;
     }
 
-    return state['features/base/config']?.disablePolls;
+    return state['features/base/config']?.disablePolls || iAmVisitor(state);
 }
