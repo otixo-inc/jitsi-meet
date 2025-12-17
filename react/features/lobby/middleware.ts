cf3a9710abf9172f0579e5d415a08da338c06237
Jamie <jamie@otixo.com>
2025-12-17 14:15:22 +0100

    Revert "stop lobby music"
    This reverts commit 0356d4a9aa28fe068b0c2c68c1755bdc75024fa9.

--- react/features/lobby/middleware.ts
+++ react/features/lobby/middleware.ts
@@ -54,8 +54,7 @@ import {
 import {
     KNOCKING_PARTICIPANT_ARRIVED_OR_UPDATED,
     KNOCKING_PARTICIPANT_LEFT,
-    SET_KNOCKING_STATE,
-    SET_LOBBY_VISIBILITY
+    SET_KNOCKING_STATE
 } from './actionTypes';
 import {
     approveKnockingParticipant,
@@ -122,15 +121,11 @@ MiddlewareRegistry.register(store => next => action => {
 
         break;
     }
-    case SET_LOBBY_VISIBILITY: {
-        if (!action.visible) {
-            store.dispatch(stopSound(LOBBY_MUSIC_SOUND_ID));
-        }
-        break;
-    }
     case SET_KNOCKING_STATE: {
         if (action.knocking) {
             store.dispatch(playSound(LOBBY_MUSIC_SOUND_ID));
+        } else {
+            store.dispatch(stopSound(LOBBY_MUSIC_SOUND_ID));
         }
         break;
     }
