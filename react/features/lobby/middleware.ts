0ee44dd3aa54722d988d02f6ab0196e69334af4f
Jamie <jamie@otixo.com>
2025-12-17 14:15:26 +0100

    Revert "fix(lobby): play lobby music when user starts knocking"
    This reverts commit 73a9cbbce55ecacdc42aef6ec76429e9ff7e7fac.

--- react/features/lobby/middleware.ts
+++ react/features/lobby/middleware.ts
@@ -54,7 +54,7 @@ import {
 import {
     KNOCKING_PARTICIPANT_ARRIVED_OR_UPDATED,
     KNOCKING_PARTICIPANT_LEFT,
-    SET_KNOCKING_STATE
+    SET_LOBBY_VISIBILITY
 } from './actionTypes';
 import {
     approveKnockingParticipant,
@@ -121,8 +121,8 @@ MiddlewareRegistry.register(store => next => action => {
 
         break;
     }
-    case SET_KNOCKING_STATE: {
-        if (action.knocking) {
+    case SET_LOBBY_VISIBILITY: {
+        if (action.visible) {
             store.dispatch(playSound(LOBBY_MUSIC_SOUND_ID));
         } else {
             store.dispatch(stopSound(LOBBY_MUSIC_SOUND_ID));
